597be8d2ec496b8c61358c3e0d2ca8f4ce256959
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-05 23:51:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:grace_hopper][:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:grace_hopper][:dennis_ritchie][:languages][1] = "Assembly"
 return programmer_hash
 end
